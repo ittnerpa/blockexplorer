30208ea79762f20598c806290b00255a8cd82649
Ittner <patrick.ittner@wechselgott.com>
2022-04-19 12:23:00 +0200

    remove unused imports

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,11 +1,7 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import Explorer from './views/Explorer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Blocks from './views/Blocks';
-import { useQuery } from 'react-query';
-import { Block } from '@cosmjs/stargate';
-import { cosmosClient } from './services/cosmosClient';
-import { BlockContext } from './store/BlockContext';
 import {useQueryBlocks} from "./services/useQueryBlocks";
 
 function Routing() {
